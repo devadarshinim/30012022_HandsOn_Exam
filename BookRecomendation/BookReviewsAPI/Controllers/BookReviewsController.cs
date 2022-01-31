2f0845eb68e58e6584ad5bdf11abe80fc204ee02
devadarshinim <devadarshinim52@gmail.com>
2022-01-31 12:27:54 +0530

    working on unit testing

---
+++ BookRecomendation/BookReviewsAPI/Controllers/BookReviewsController.cs
@@ -0,0 +1,40 @@
+﻿using BookRecomendationBusinessLayer;
+using BookRecomendationDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookReviewsAPI.Controllers
+{
+    public class BookReviewsController : ApiController
+    {
+        BookRecomendationBL blobj;
+        public BookReviewsController()
+        {
+            blobj = new BookRecomendationBL();
+        }
+        [HttpGet]
+        public HttpResponseMessage GetRatingsForBook()
+        {
+            try
+            {
+                List<BookDTO> list = blobj.ShowReviewsForBook();
+                if (list.Count > 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, list);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.OK, "error");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.OK, "Catch");
+            }
+        }
+    }
+}
---
+++ BookRecomendation/BookReviewsAPITests/Controllers/BookReviewsControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookReviewsAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookReviewsAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class BookReviewsControllerTests
+    {
+        [TestMethod()]
+        public void GetRatingsForBookTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
